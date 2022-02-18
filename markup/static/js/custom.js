fb6bb332b75289f854c039af456c7f73720e2d9f
User <uriyderechey@gmail.com>
2022-02-18 17:46:33 +0200

    New design

--- markup/static/js/custom.js
+++ markup/static/js/custom.js
@@ -1,5 +1,52 @@
 $(document).ready(function () {
 
+    var $burger = $('.js-header-burger');
+    var $nav = $('.js-header-toggle nav');
+    $burger.on('click', function () {
+        $(this).toggleClass('open');
+        $('.header').toggleClass('open');
+        $nav.toggleClass('open');
+        $('body').toggleClass('fixed-position');
+    });
+
+    $('.header__menu-item a').click(function () {
+        $('a.active').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.js-to-item').on('click', function () {
+        scrollToItem($(this));
+        $('.js-header-burger').removeClass('open');
+        $('.header').removeClass('open');
+        $('.js-header-toggle nav').removeClass('open');
+        $('.header__burger-letter').removeClass('open');
+        $('.header__nav').removeClass('open');
+        $('body').removeClass('fixed-position');
+    });
+
+    $(window).scroll(function () {
+        const height = $(window).scrollTop();
+
+        if (height > 1) {
+            $('.js-header').addClass('is-scroll');
+        } else {
+            $('.js-header').removeClass('is-scroll');
+        }
+
+        var $sections = $('section');
+        $sections.each(function (i, el) {
+            var top = $(el).offset().top - 100;
+            var bottom = top + $(el).height();
+            var scroll = $(window).scrollTop();
+            var id = $(el).attr('id');
+            if (scroll > top && scroll < bottom) {
+                $('a.active').removeClass('active');
+                $('a[href="#' + id + '"]').addClass('active');
+            }
+        });
+    });
+
+
     // slider
     if ($('.js-slider').length) {
         $('.js-slider').slick({
